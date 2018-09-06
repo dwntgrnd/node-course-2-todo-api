8a8dc3681db781c8da6301675187852becd5c971
Doren Berge <doren@downtoground.com>
2018-09-06 14:36:59 -0400

    add delete scripts

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID()
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err){
